c690b1c6f52fa300170a0034995f0e5e98e51921
gyeongnam11 <rrudska11@gmail.com>
2021-08-13 02:06:17 +0900

    A-1 menu slide

--- A-1 - just 쇼핑몰 - 메뉴/script/common.js
+++ A-1 - just 쇼핑몰 - 메뉴/script/common.js
@@ -1,13 +1,36 @@
-$(document).ready(function () {
-
-    $(".menu > ul > li").mouseover(function () {
+//menu
+$(document).ready(function(){
+    
+    $(".menu > ul > li").mouseover(function (){
         $(".menu > ul > li > ul").stop().slideDown(200);
     });
-
-    $(".menu > ul > li").mouseout(function () {
-        $(".menu > ul > li > ul").stop().slideUp(200);
+    $(".menu > ul > li").mouseout(function (){
+        $(".menu >ul> li > ul").stop().slideUp(200);
     });
 });
+
+//slide
+$(document).ready(function(){
+    function slider(){
+        var num =0;
+        var result;
+        setInterval (function(){
+            result = num * -300;
+            num++;
+            console .log(num);
+            if(num==3){
+                num=0;
+            }
+            console .log(num);
+            $(".slider").stop().animate({
+                top : result + 'px'
+            });
+        },1000);
+    }
+    slider();
+});
+
+
 
 
 
