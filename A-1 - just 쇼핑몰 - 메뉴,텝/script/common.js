1bc892153db7988002c6b0ea91adcde6028225ac
gyeongnam11 <rrudska11@gmail.com>
2021-08-14 02:09:28 +0900

    A-1 tab

--- A-1 - just 쇼핑몰 - 메뉴,텝/script/common.js
+++ A-1 - just 쇼핑몰 - 메뉴,텝/script/common.js
@@ -29,6 +29,24 @@ $(document).ready(function(){
     }
     slider();
 });
+//tab bt
+$(document).ready(function () {
+    var tabBtn = $(".tab_btn > ul > li");
+    var tabCon = $(".tab_con > div");
+
+    tabCon.hide().eq(0).show(); 
+
+    tabBtn.click(function (e) {
+        e.preventDefault();
+        var target = $(this);
+        var index = target.index();
+        //        alert(index);
+        tabBtn.removeClass("actived_tab");
+        target.addClass("actived_tab");
+        tabCon.css("display", "none");
+        tabCon.eq(index).css("display", "block");
+    })
+});
 
 
 
