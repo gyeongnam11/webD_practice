8b8a0e7c9276a99a1b0992292a36fe294fd3f286
gyeongnam11 <rrudska11@gmail.com>
2021-08-18 00:22:02 +0900

    가로메뉴 슬라이드

---
+++ A-2 -그린 복지재단 가로메뉴2,좌우슬라이드/script/common.js
@@ -0,0 +1,61 @@
+//menu
+$(document).ready(function () {
+    $(".menu>ul>li").mouseover(function () {
+            $(".menu>ul>li>ul").show();
+        }),
+        $(".menu>ul>li").mouseout(function () {
+            $(".menu>ul>li>ul").hide();
+        });
+});
+
+//slide
+$(document).ready(function () {
+    function slider() {
+        var num = 0;
+        var result;
+        setInterval(function () {
+            result = num * -1200;
+            num++;
+            console.log(num);
+            console.log(result);
+            if (num == 3) {
+                num = 0;
+            }
+            console.log(num);
+            $(".slider").stop().animate({
+                left: result + 'px'
+            });
+        }, 1000);
+    }
+    slider();
+});
+
+////tab
+//$(document).ready(function () {
+//    var tabBtn = $(".tab_btn>ul>li");
+//    var tabCon = $(".tab_con>div");
+//
+//    tabCon.hide().eq(0).show();
+//
+//    tabBtn.click(function (e) {
+//        e.preventDefault();
+//
+//        var target = $(this);
+//        var index = target.index();
+//
+//        tabCon.css("display", "none");
+//        tabCon.eq(index).css("display", "block");
+//    })
+//});
+
+//popup
+$(document).ready(function () {
+    $(".tab_con1 li:first").click(function () {
+        $(".layer").show();
+        $(".layer-bg").show();
+    });
+    $(".close-btn").click(function () {
+        $(".layer").hide();
+        $(".layer-bg").hide();
+    });
+});
