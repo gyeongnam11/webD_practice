f57c25ca0bb4c6f6187f3ac90e7fa0ee3607528f
gyeongnam11 <rrudska11@gmail.com>
2021-08-21 02:55:23 +0900

    가로메뉴2, fadein out

---
+++ 해변 빛축제-가로메뉴2 fadein, out/script/common.js
@@ -0,0 +1,44 @@
+//menu
+$(document).ready(function(){
+    $(".menu>ul>li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown(200);
+    });
+    $(".menu>ul>li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp(200);
+    });
+});
+
+//slide
+$(document).ready(function(){
+    function slider(){
+        var num=0;
+        setInterval(function(){
+            if(num==3){
+                num=0;
+            }
+            $('.slider p').hide();
+            $('.slider p').eq(-num).stop().fadeOut();
+            $('.slider p').eq(-num).stop().fadeIn();
+            console.log(num);
+            num++;
+        },2000);
+    }
+    slider();
+});
+
+//tab
+$(document).ready(function(){
+    var tabBtn = $(".tab_btn>ul>li");
+    var tabCon = $(".tab_con>div");
+    
+    tabCon.hide().eq(0).show();
+    
+    tabBtn.click(function(e){
+        e.preventDefault();
+        
+        var target = $(this);
+        var index = target.index();
+        tabCon.css("display","none");
+        tabCon.eq(index).css("display","block");
+    })
+});
