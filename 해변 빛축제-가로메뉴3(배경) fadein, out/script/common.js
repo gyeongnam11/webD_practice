e74d260e3513ce83b8938554414134ffc47efeb0
gyeongnam11 <rrudska11@gmail.com>
2021-08-21 16:19:47 +0900

    가로메뉴3 배경, fadein out

---
+++ 해변 빛축제-가로메뉴3(배경) fadein, out/script/common.js
@@ -0,0 +1,60 @@
+//menu
+$(document).ready(function () {
+    $(".menu>ul>li").mouseover(function () {
+            $(".menu>ul>li>ul").stop().slideDown(200);
+            $(".menu_bg").stop().slideDown(200);
+        }),
+        $(".menu>ul>li").mouseout(function () {
+            $(".menu>ul>li>ul").stop().slideUp(200);
+            $(".menu_bg").stop().slideUp(200);
+        });
+
+});
+
+//slide
+$(document).ready(function(){
+    function slider(){
+        var num = 0;
+        setInterval(function(){
+            if(num==3){
+                num=0;
+            }
+            $('.slider p').hide();
+            $('.slider p').eq(-num).stop().fadeOut();
+            $('.slider p').eq(-num).stop().fadeIn();
+            console.log(num);
+            num++;
+        },1000);
+    }
+    slider();
+});
+
+//tab
+$(document).ready(function(){
+    var tabBtn = $(".tab_btn>ul>li");
+    var tabCon = $(".tab_con>div");
+    
+    tabCon.hide().eq().show();
+    tabBtn.click(function(e){
+        e.preventDefault();
+        
+        var target = $(this);
+        var index = target.index();
+        
+        tabCon.css("display","none");
+        tabCon.eq(index).css("display","none");
+    })
+});
+
+
+//popup
+$(document).ready(function(){
+    $(".:first").click(function(){
+        (".layer").show();
+        (".layer_bg").show();
+    });
+    $(".close_btn").click(function(){
+        (".layer").hide();
+        (".layer_bg").hide();
+    });
+});
