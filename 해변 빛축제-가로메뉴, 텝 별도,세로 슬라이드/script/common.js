b259167baf03ea61f340cd7acab77444cec3d16d
gyeongnam11 <rrudska11@gmail.com>
2021-08-23 00:57:29 +0900

    가로메뉴 상하슬라이드

---
+++ 해변 빛축제-가로메뉴, 텝 별도,세로 슬라이드/script/common.js
@@ -0,0 +1,41 @@
+//menu
+$(document).ready(function () {
+    $(".menu>ul>li").mouseover(function () {
+        $(".menu>ul>li>ul").stop().slideDown(200);
+    });
+    $(".menu>ul>li").mouseout(function () {
+        $(".menu>ul>li>ul").stop().slideUp(200);
+    });
+});
+
+//slider
+$(document).ready(function () {
+    function slider() {
+        var num = 0;
+        var result;
+        setInterval(function () {
+            result = num * -300;
+            num++;
+            console.log(num);
+            if (num == 3) {
+                num = 0;
+            }
+            console.log(num);
+            $(".slider").stop().animate({
+                top: result + 'px'
+            });
+        }, 2000);
+    }
+    slider();
+});
+//popup
+$(document).ready(function () {
+    $(".tab_con li:first").click(function () {
+        $(".layer").show();
+        $(".layer_bg").show();
+    });
+    $(".close_btn").click(function () {
+        $(".layer").hide();
+        $(".layer_bg").hide();
+    });
+});
