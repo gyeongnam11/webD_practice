ff0aa054350e45ca66a78499047348dd5a49521b
gyeongnam11 <rrudska11@gmail.com>
2021-08-30 00:56:25 +0900

    세로메뉴

---
+++ 대한천문대 - 세로메뉴1,2,tab별도/script/common.js
@@ -0,0 +1,66 @@
+//menu
+$(document).ready(function () {
+    $(".menu>ul>li").hover(
+        function () {
+            $(this).find(".submenu").stop().slideDown(200);
+        },
+        function () {
+            $(this).find(".submenu").stop().slideUp(200);
+        }
+    );
+});
+
+//slide
+//$(document).ready(function () {
+//    function slider() {
+//        var num = 0;
+//        var result;
+//        setInterval(function () {
+//            result = num * -350;
+//            num++;
+//            console.log(num);
+//            if (num == 3) {
+//                num = 0;
+//            }
+//            console.log(num);
+//            $(".slider").stop().animate({
+//                top: result + 'px'
+//            });
+//        }, 2000);
+//    }
+//    slider();
+//});
+
+$(document).ready(function () {
+    function slider() {
+        var num = 0;
+
+        setInterval(function () {
+            if (num == 3) {
+                num = 0;
+            }
+            $('.slider p').hide();
+            $('.slider p').eq(-num).stop().fadeOut();
+            $('.slider p').eq(-num).stop().fadeIn();
+            console.log(num);
+            num++;
+
+        }, 1000);
+    }
+    slider();
+});
+
+
+
+
+//popup
+$(document).ready(function () {
+    $(".tab_con li:first").click(function () {
+        $(".layer").show();
+        $(".layer_bg").show();
+    });
+    $(".close_btn").click(function () {
+        $(".layer").hide();
+        $(".layer_bg").hide();
+    });
+});
