fc8e6fb8410e42afd14e91a6f3bcf75db9c9beea
gyeongnam11 <rrudska11@gmail.com>
2021-08-31 01:14:40 +0900

    가로메뉴

---
+++ 대한천문대 - 가로메뉴배경 좌우슬라이드,텝 별도2/script/common.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $(".menu>ul>li").mouseover(function () {
+        $(".menu>ul>li>ul").stop().slideDown(200);
+        $(".menu_bg").stop().slideDown(200);
+    });
+    $(".menu>ul>li").mouseout(function () {
+        $(".menu>ul>li>ul").stop().slideUp(200);
+        $(".menu_bg").stop().slideUp(200);
+    });
+});
+
+/*$(document).ready(function () {
+    $(".menu>ul>li").mouseover(function () {
+        $(this).find(".submenu").stop().slideDown(200);
+    });
+    $(".menu>ul>li").mouseout(function () {
+        $(this).find(".submenu").stop().slideUp(200);
+    });
+});*/
+
+$(document).ready(function () {
+    function slider() {
+        var num = 0;
+        var result;
+        setInterval(function () {
+            result = num * -1200;
+            num++;
+            console.log(num);
+            console.log(result);
+            if (num == 3) {
+                num = 0;
+            }
+            console.log(num);
+            $(".slider").stop().animate({
+                left: result + 'px'
+            });
+        }, 2000);
+    }
+    slider();
+});
+
+$(document).ready(function () {
+    var tabBtn = $(".tab_btn>ul>li");
+    var tabCon = $(".tab_con>div");
+
+    tabCon.hide().eq(0).show();
+
+    tabBtn.click(function (e) {
+        e.preventDefault();
+
+        var target = $(this);
+        var index = target.index();
+
+        tabCon.css("display", "none");
+        tabCon.eq(index).css("display", "block");
+    });
+});
+
+$(document).ready(function () {
+    $(".tab_con li:first").click(function () {
+        $(".layer").show();
+        $(".layer_bg").show();
+    });
+    $(".close_btn").click(function () {
+        $(".layer").hide();
+        $(".layer_bg").hide();
+    });
+});
